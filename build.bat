5889fe7fc3505b33b7316ef3b981d02b02cf13cf
Atul Kumar <5877089+atulkumar2@users.noreply.github.com>
2026-02-19 23:32:41 +0530

    Removed build scripts from top level

--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Backward compatibility wrapper - calls the actual script in scripts folder
-powershell -ExecutionPolicy Bypass -File "%~dp0scripts\build.ps1" %*
--- build.ps1
+++
@@ -1,2 +0,0 @@
-param([switch]$SkipPause)
-& "$PSScriptRoot\scripts\build.ps1" @PSBoundParameters
